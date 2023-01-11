a7c3492b58b68378fc7c59bd4aa2d2da53875827
mike <barbeque@users.noreply.github.com>
2023-01-11 14:52:04 -0700

    C-language output

--- tools/icn-generator/icn-generator.py
+++ tools/icn-generator/icn-generator.py
@@ -22,19 +22,54 @@ im = im.convert("1")
 def padded_hex(byte):
     return "{:02x}".format(byte)
 
-# Output the pixel data
-def output_image(im):
-    byte_count = 0
+def get_bytes_for_image(im):
+    # get the apple format for ICN# (8 pixels per byte)
+    # in an array of bytes
+    output = []
     for y in range(0, im.height):
         for x in range(0, im.width // 8):
             this_byte = 0
             for bit in range(0, 8):
                 this_byte = (this_byte << 1) | (im.getpixel((x * 8 + bit, y)) & 0x01)
-            print(padded_hex(this_byte), end = " ")
-            byte_count += 1
-    print() # newline to clear the screen
-    assert(byte_count == 128)
-    
-output_image(im, options)
+            output.append(this_byte)
+    return output
+
+# Output the pixel data in raw hex
+def output_image_hex(im, mask):
+    def print_image_as_hex(i):
+        bytes = get_bytes_for_image(i)
+        assert(len(bytes) == 128)
+        for i in range(0, len(bytes)):
+            print(padded_hex(bytes[i]), end = " ")
+        print() # put a newline between entries (remove this if it causes problems)
+
+    print_image_as_hex(im)
+    print_image_as_hex(mask)
+
+def output_image_c(im, mask):
+    # basically the same as the other code, but in C format
+    im_bytes = get_bytes_for_image(im)
+    mask_bytes = get_bytes_for_image(mask)
+    appended = im_bytes + mask_bytes
+
+    print(f'static const unsigned char icon[] = {{ /* {args[0]} */')
+    # TODO: pretty print this so it's not just one big glob
+    print(', '.join([ "0x{:02x}".format(b) for b in appended ]))
+    print('};')
+    return
+
+def generate_mask(im):
+    mask = im.copy()
+    # FIXME: eventually do some fancy outline detection, or
+    # flood fill and invert if the outline isn't literally touching
+    # the edges like my example one is
+    return mask
+
+mask = generate_mask(im)
+
+if options.generate_c:
+    output_image_c(im, mask)
+else:
+    output_image_hex(im, mask)
 
 # Attempt to guess the mask as well (flood-fill then invert?)
